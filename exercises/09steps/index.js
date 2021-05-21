b65ed6799a2473943db26e6939c10779066bebc7
Hardik Patel <emailhardikpatel@gmail.com>
2021-05-21 10:42:37 -0700

    Steps

--- exercises/09steps/index.js
+++ exercises/09steps/index.js
@@ -24,12 +24,17 @@ function steps(n) {
     let secondPart = ``;
     if (n > 1) {
         secondPart = ` `;
-     }
+    }
 
     for (let i = 0; i < n; i++) {
         console.log(firstPart.repeat(i + 1) + secondPart.repeat(n - i - 1));
     }
 }
 
-steps(2);
-module.exports = steps;
+const stepsTwo = n => {
+    for (let i = 1; i < n + 1; i++) {
+        console.log('#'.repeat(i) + ' '.repeat(n - i));
+    }
+};
+
+module.exports = stepsTwo;
